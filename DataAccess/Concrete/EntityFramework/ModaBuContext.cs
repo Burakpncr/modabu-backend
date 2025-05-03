eb8e5e8d8f639fa2e14bfb48959499329a2cd0fb
Burakpncr <burakpancar0535@gmail.com>
2025-05-04 00:38:07 +0300

    Azure yayını için connection string temizlendi

--- DataAccess/Concrete/EntityFramework/ModaBuContext.cs
+++ DataAccess/Concrete/EntityFramework/ModaBuContext.cs
@@ -12,8 +12,13 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ModaBu;Trusted_Connection=true");
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = "Server=tcp:modabusqlserver.database.windows.net,1433;Initial Catalog=ModaBuDb01;Persist Security Info=False;User ID=sqladmin;Password=Bu.05.rak35;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
         }
+
         public DbSet<Product> Products { get; set; }
 
     }
