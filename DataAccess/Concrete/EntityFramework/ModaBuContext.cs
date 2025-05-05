f4cf22c43a8b8377c0040da1a491077f6896a27c
Burakpncr <burakpancar0535@gmail.com>
2025-05-05 12:52:59 +0300

    Connection string yapılandırması güncellendi, Azure uyumu sağlandı

--- DataAccess/Concrete/EntityFramework/ModaBuContext.cs
+++ DataAccess/Concrete/EntityFramework/ModaBuContext.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Entities.Concrete;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +12,39 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class ModaBuContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public ModaBuContext() { }
+
+        // 2) DI ile gelen ctor (senin zaten vardı)
+        public ModaBuContext(DbContextOptions<ModaBuContext> options)
+            : base(options)
         {
-            optionsBuilder.UseSqlServer("Server=tcp:modabusqlserver.database.windows.net,1433;Initial Catalog=ModaBuDb01;Persist Security Info=False;User ID=sqladmin;Password=Bu.05.rak35;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
 
+        // 3) Parametresiz kullanım için OnConfiguring’i geri ekle
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // appsettings.json’dan veya ENV’den oku
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+
+                var cs = config.GetConnectionString("DefaultConnection");
+                if (!string.IsNullOrWhiteSpace(cs))
+                {
+                    optionsBuilder.UseSqlServer(cs);
+                }
+                // yoksa istersen burada sabit bir connection string de koyabilirsin
+            }
+        }
+
+        // -- DbSet’lerin
         public DbSet<Product> Products { get; set; }
         public DbSet<OperationClaim> OperationClaims { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
-
     }
 }
